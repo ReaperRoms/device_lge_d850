be5e1f3bd70f045b0fe718741d277b471b422361
roguesyko <roguesyko@gmail.com>
2016-10-17 13:31:59 -0400

    Broke dat mothafuckin shit

--- broken.mk
+++ broken.mk
@@ -1,15 +1,19 @@
 $(call inherit-product, device/lge/d850/full_d850.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_d850
+PRODUCT_NAME := broken_d850
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_att_us" \
     BUILD_FINGERPRINT="lge/g3_att_us/g3:4.4.2/KVT49L.D85010d/D85010d.1403173065:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_att_us-user 4.4.2 KVT49L.D85010d D85010d.1403173065 release-keys"
+
+# Broken Device Maintainers
+    PRODUCT_BUILD_PROP_OVERRIDES += \
+  	DEVICE_MAINTAINERS="Joe (roguesyko) Repasy"
